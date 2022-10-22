ac6b7b9e4f868b112d134d88b1f4e3fc5bbc2ca5
Brankonymous <brankogrbic01@gmail.com>
2022-10-22 11:31:37 +0200

    Walk debuging

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
+    private CircleCollider2D circleCollider2D;
+    private RaycastHit2D hit_x, hit_y;
     private Vector3 moveDelta;
 
     public float moveForce = 1.5f;
@@ -12,7 +14,7 @@ public class Player : MonoBehaviour
     private Animator anim;
     private string WALK_ANIMATION = "Walk";
 
-     private int _attack = 6;
+    private int _attack = 6;
     private int _defense = 6;
     private int _maximumHealth = 100;
 
@@ -43,6 +45,7 @@ public class Player : MonoBehaviour
 
     private void movePlayer() 
     {
+        // Get info for moving
         Vector3 originalScale = transform.localScale;
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
@@ -54,7 +57,44 @@ public class Player : MonoBehaviour
         else if (moveDelta.x < 0 && originalScale.x > 0)
             transform.localScale = Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
 
-        transform.Translate(moveDelta * Time.deltaTime * moveForce);
+        // Meke sure we can move in x and y direction
+        hit_x = Physics2D.BoxCast(
+            transform.position, 
+            boxCollider.size, 
+            0,
+            new Vector2(moveDelta.x, 0), 
+            Mathf.Abs(moveDelta.x * Time.deltaTime * moveForce),
+            LayerMask.GetMask("Fighter")
+            );
+        hit_y = Physics2D.BoxCast(
+            transform.position, 
+            boxCollider.size, 
+            0, 
+            new Vector2(0, moveDelta.y), 
+            Mathf.Abs(moveDelta.y * Time.deltaTime * moveForce),
+            LayerMask.GetMask("Fighter")
+            );
+
+        if (hit_x.collider != null || hit_y.collider != null)
+        {
+            Debug.Log("AHA");
+        }
+
+        /*
+        Physics2D.CircleCast(
+            transform.position, 
+            circleCollider2D.radius, 
+            new Vector2(Mathf.Abs(moveDelta * Time.deltaTime * moveForce))
+        );
+        */
+
+        if (hit_x.collider == null && hit_y.collider == null)
+        {
+            // We can move
+            transform.Translate(moveDelta * Time.deltaTime * moveForce);
+
+        }
+
     }
 
     private void attackPlayer()
