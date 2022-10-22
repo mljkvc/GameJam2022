9fc2c84cce78a9ce604884fd5b77420033abed63
Marko <moljkovic5@gmail.com>
2022-10-22 16:24:16 +0200

    inventori
    promenio inventori na sva 3 nivoa

--- Assets/Scripts/inventori/Inventory_UI.cs
+++ Assets/Scripts/inventori/Inventory_UI.cs
@@ -6,27 +6,30 @@ public class Inventory_UI : MonoBehaviour
 {
     public GameObject inventori;
 
+    private void Start()
+    {
+        inventori.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
+        {
             otvoriInventori();
+        }
     }
 
     public void otvoriInventori()
     {
         if (!inventori.activeSelf)
+        {
             inventori.SetActive(true);
-
+        }
         else
-            inventori.SetActive(false); 
-
-    }
-
-    void Setup()
-    {
-
+        {
+            inventori.SetActive(false);
+        }
     }
 
 }
