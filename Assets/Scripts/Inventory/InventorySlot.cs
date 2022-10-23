147faa2ab8545fdd4381dc427eec9c0f9ccff5e2
ZekiMilovan001 <116019811+ZekiMilovan001@users.noreply.github.com>
2022-10-23 02:04:54 +0200

    a

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
@@ -13,20 +14,30 @@ public class InventorySlot : MonoBehaviour
 
 
     // Start is called before the first frame update
+
+
     void Start()
     {
         btn = GetComponent<Button>();
-        btn.onClick.AddListener(() => { OnClickAction(); });
+        btn.onClick.AddListener(() => {
+            Debug.Log("STISNUTO");
+            OnClickAction(); });   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
-    private void OnClickAction()
+    public void OnClickAction()
     {
-        item.inventoryAction();
+        
+        Debug.Log("POZVANO");
+        switch (this.item.itemType)
+        {
+            case Item.ItemType.Potion: heal(); break;
+        }
+        Debug.Log("POZVANO");
     }
     public void refresh()
      {
@@ -48,4 +59,35 @@ public class InventorySlot : MonoBehaviour
         txt.text = "";
             //item.amount.ToString();
     }
+    private void heal()
+    {
+        Player player = Transform.FindObjectOfType<Player>();
+
+        player.health = Math.Min(100, player.health + 25);
+    }
+
+
+
+
+    private int whatKey(KeyCode key)
+    {
+        switch (key)
+        {
+            case KeyCode.Alpha1: return 1;
+            case KeyCode.Alpha2: return 2;
+            case KeyCode.Alpha3: return 3;
+            case KeyCode.Alpha4: return 4;
+            case KeyCode.Alpha5: return 5;
+            case KeyCode.Alpha6: return 6;
+            case KeyCode.Alpha7: return 7;
+            case KeyCode.Alpha8: return 8;
+            case KeyCode.Alpha9: return 9;
+
+        }
+
+
+        return -1;
+    }
+
+
 }
