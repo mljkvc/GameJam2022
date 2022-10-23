b04d4eb92d606f7a1aa61aafbe131bac50e4e35c
Marko <moljkovic5@gmail.com>
2022-10-23 12:38:11 +0200

    ded scin
    jsooo

--- Assets/Scripts/death/gameover.cs
+++ Assets/Scripts/death/gameover.cs
@@ -6,19 +6,31 @@ using UnityEngine.UI;
 
 public class gameover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject menu;
+
+    private float health = Player.health;
+    private void Start()
     {
-        
+        menu.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
+        Debug.Log(health);
+        if(health <= 0)
+        {
+            health = 0;
+            menu.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
+
     public void idiStart()
     {
         //uzima trenutni br scene i ide +1 na sledecu scenu
+        Time.timeScale = 1f;
+        health = 100f;
         SceneManager.LoadScene(0);
     }
 
